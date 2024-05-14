6e2a908e9e97015d142bfea4dcbc0c3278b98406
Maksym-Ocheretniuk <114191625+Maksym-Ocheretniuk@users.noreply.github.com>
2024-05-14 12:57:47 +0000

    Deploying to gh-pages from @ Maksym-Ocheretniuk/renovate-charity@dc4c38996c4254acf07696f9e52a51c0cf948869 🚀

--- index.2bb7ba16.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".mySwiper",{effect:"coverflow",grabCursor:!0,centeredSlides:!0,slidesPerView:"auto",coverflowEffect:{rotate:50,stretch:0,depth:100,modifier:1,slideShadows:!0},pagination:{el:".swiper-pagination"}});
-//# sourceMappingURL=index.2bb7ba16.js.map
---
+++ index.a55741e7.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{spaceBetween:30,effect:"fade",navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",clickable:!0},autoplay:{delay:4500,disableOnInteraction:!1}});
+//# sourceMappingURL=index.a55741e7.js.map
