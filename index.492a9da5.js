a6d51448a4f3aeb0d42f12931459fa5043392480
Maksym-Ocheretniuk <114191625+Maksym-Ocheretniuk@users.noreply.github.com>
2024-05-22 10:11:58 +0000

    Deploying to gh-pages from @ Maksym-Ocheretniuk/renovate-charity@c1765e5f225d693fa0b1e3f707b6e0efc1d7c121 🚀

--- index.492a9da5.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".mySwiper",{spaceBetween:30,effect:"fade",navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",clickable:!0},autoplay:{delay:4500,disableOnInteraction:!1}});
-//# sourceMappingURL=index.492a9da5.js.map
