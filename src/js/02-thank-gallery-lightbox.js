1dff13df3c98bd0ce837783fb47dae39acf3ce88
Maksym Ocheretniuk <114191625+Maksym-Ocheretniuk@users.noreply.github.com>
2024-06-03 19:39:32 +0300

    page with fund thanks

---
+++ src/js/02-thank-gallery-lightbox.js
@@ -0,0 +1,44 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { thanksGalleryItems } from './02-gallery-thank-items';
+
+console.log(thanksGalleryItems);
+
+// Знаходимо галерею та її елементи "li"
+const thanksGalleryEl = document.querySelector('.thanks-gallery');
+
+// Додаємо розмітку в структуру
+thanksGalleryEl.innerHTML = createListImg(thanksGalleryItems);
+
+// Функція створення розмітки "li" списку в HTML
+function createListImg(images) {
+  return images
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="thank-gallery__item">
+            <a class="thank-gallery__link" href="${original}">
+                <img
+                    class="thank-gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                    loading="lazy"
+                    width="360px" 
+                    height="360px"
+                />
+            </a>
+        </li>`
+    )
+    .join('');
+}
+
+// бібліотека SimpleLightbox
+const lightbox = new SimpleLightbox('.thanks-gallery a', {
+  // captions: false,
+  // captionsData: 'alt',
+  // captionDelay: 1000,
+  // captionPosition: top,
+  // nav: true,
+  // close: true,
+  // overlay: true,
+});
