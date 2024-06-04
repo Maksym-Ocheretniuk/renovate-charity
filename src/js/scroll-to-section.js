2ec044ff830ad4234641e104fa92abd763824f65
Maksym Ocheretniuk <114191625+Maksym-Ocheretniuk@users.noreply.github.com>
2024-06-04 16:40:37 +0300

    upd scrolling to section - by ID and JS

--- src/js/scroll-to-section.js
+++ src/js/scroll-to-section.js
@@ -5,26 +5,41 @@
 // "data-goto=".scroll__data-4" - стороінка "unit-reconstruction" (Відбудова країни)
 // "data-goto=".scroll__data-5" - секція "HEADER" (кнопка "Контакти")
 
-const reconstructionButtons = document.querySelectorAll('.reconstruction__btn[data-goto]') || document.querySelectorAll('.reconstruction__btn-title[data-goto]') || document.querySelectorAll('.reconstruction__btn-city[data-goto]');
-//  || document.querySelectorAll('.menu__link[data-goto]')
+// const scrollButtons = document.querySelectorAll('.reconstruction__btn[data-goto]') || document.querySelectorAll('.reconstruction__btn-title[data-goto]') || document.querySelectorAll('.reconstruction__btn-city[data-goto]') || document.querySelectorAll('.menu__link[data-goto]');
 
-// console.log(reconstructionButtons)
+// const Buttons = document.querySelectorAll('.reconstruction__btn[data-goto]') || document.querySelectorAll('.reconstruction__btn-title[data-goto]') || document.querySelectorAll('.reconstruction__btn-city[data-goto]');
+// const scrollButtons = Buttons.push("document.querySelectorAll('.menu__link[data-goto]')")
+// console.log("Buttons: ", Buttons);
 
-if (reconstructionButtons.length > 0) {
-  reconstructionButtons.forEach(reconstructionButton => {
-    reconstructionButton.addEventListener("click", onReconstructionClick);
+const scrollButtons = document.querySelectorAll('.menu__link[data-goto]');
+
+console.log("scrollButtons: ", scrollButtons);
+
+if (scrollButtons.length > 0) {
+  scrollButtons.forEach(scrollButton => {
+    scrollButton.addEventListener("click", onScrollButtonClick);
     }
   )
 
-  function onReconstructionClick(e) {
+  function onScrollButtonClick(e) {
     e.preventDefault();
     
-    const reconstructionButton = e.target;
+    const scrollButton = e.target;
     
-    if (reconstructionButton.dataset.goto && document.querySelector(reconstructionButton.dataset.goto)) {
+    if (scrollButton.dataset.goto && document.querySelector(scrollButton.dataset.goto)) {
 
-      const gotoBlock = document.querySelector(reconstructionButton.dataset.goto);
+      const gotoBlock = document.querySelector(scrollButton.dataset.goto);
       const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
+
+      // закривання меню
+      const iconMenu = document.querySelector('.menu__icon')
+      const menuBody = document.querySelector('.menu__body')
+
+      if (iconMenu.classList.contains('_active')) {
+        document.body.classList.remove('_lock')
+        iconMenu.classList.remove('_active')
+        menuBody.classList.remove('_active')
+      }
 
       window.scrollTo({
         top: gotoBlockValue,
