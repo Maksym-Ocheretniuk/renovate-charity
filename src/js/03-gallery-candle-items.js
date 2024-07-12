0c1b198f92cd22a1d82f3de6f7a134e5c082683d
Maksym Ocheretniuk <114191625+Maksym-Ocheretniuk@users.noreply.github.com>
2024-07-12 17:31:06 +0300

    upd candle gallery

--- src/js/03-gallery-candle-items.js
+++ src/js/03-gallery-candle-items.js
@@ -2,7 +2,36 @@ import Candle_01_prev from '../images/candle/img-projects-candle-001@1x.webp';
 import Candle_01_orig from '../images/candle/img-projects-candle-001@2x.webp';
 import Candle_02_prev from '../images/candle/img-projects-candle-002@1x.webp';
 import Candle_02_orig from '../images/candle/img-projects-candle-002@2x.webp';
-
+import Candle_03_prev from '../images/candle/img-projects-candle-003@1x.webp';
+import Candle_03_orig from '../images/candle/img-projects-candle-003@2x.webp';
+import Candle_04_prev from '../images/candle/img-projects-candle-004@1x.webp';
+import Candle_04_orig from '../images/candle/img-projects-candle-004@2x.webp';
+import Candle_05_prev from '../images/candle/img-projects-candle-005@1x.webp';
+import Candle_05_orig from '../images/candle/img-projects-candle-005@2x.webp';
+import Candle_06_prev from '../images/candle/img-projects-candle-006@1x.webp';
+import Candle_06_orig from '../images/candle/img-projects-candle-006@2x.webp';
+import Candle_07_prev from '../images/candle/img-projects-candle-007@1x.webp';
+import Candle_07_orig from '../images/candle/img-projects-candle-007@2x.webp';
+import Candle_08_prev from '../images/candle/img-projects-candle-008@1x.webp';
+import Candle_08_orig from '../images/candle/img-projects-candle-008@2x.webp';
+import Candle_09_prev from '../images/candle/img-projects-candle-009@1x.webp';
+import Candle_09_orig from '../images/candle/img-projects-candle-009@2x.webp';
+import Candle_10_prev from '../images/candle/img-projects-candle-010@1x.webp';
+import Candle_10_orig from '../images/candle/img-projects-candle-010@2x.webp';
+import Candle_11_prev from '../images/candle/img-projects-candle-011@1x.webp';
+import Candle_11_orig from '../images/candle/img-projects-candle-011@2x.webp';
+import Candle_12_prev from '../images/candle/img-projects-candle-012@1x.webp';
+import Candle_12_orig from '../images/candle/img-projects-candle-012@2x.webp';
+import Candle_13_prev from '../images/candle/img-projects-candle-013@1x.webp';
+import Candle_13_orig from '../images/candle/img-projects-candle-013@2x.webp';
+import Candle_14_prev from '../images/candle/img-projects-candle-014@1x.webp';
+import Candle_14_orig from '../images/candle/img-projects-candle-014@2x.webp';
+import Candle_15_prev from '../images/candle/img-projects-candle-015@1x.webp';
+import Candle_15_orig from '../images/candle/img-projects-candle-015@2x.webp';
+import Candle_16_prev from '../images/candle/img-projects-candle-016@1x.webp';
+import Candle_16_orig from '../images/candle/img-projects-candle-016@2x.webp';
+import Candle_17_prev from '../images/candle/img-projects-candle-017@1x.webp';
+import Candle_17_orig from '../images/candle/img-projects-candle-017@2x.webp';
 
 export const candlesGalleryItems = [
   {
@@ -13,6 +42,80 @@ export const candlesGalleryItems = [
   {
     preview: Candle_02_prev,
     original: Candle_02_orig,
+    description: "БФ «Заново». Фото окопних свічок",
+  },
+  {
+    preview: Candle_03_prev,
+    original: Candle_03_orig,
+    description: "БФ «Заново». Фото окопних свічок",
+  },
+  {
+    preview: Candle_04_prev,
+    original: Candle_04_orig,
+    description: "БФ «Заново». Фото окопних свічок",
+  },{
+    preview: Candle_05_prev,
+    original: Candle_05_orig,
+    description: "БФ «Заново». Фото окопних свічок",
+  },
+  {
+    preview: Candle_06_prev,
+    original: Candle_06_orig,
+    description: "БФ «Заново». Фото окопних свічок",
+  },
+  {
+    preview: Candle_07_prev,
+    original: Candle_07_orig,
+    description: "БФ «Заново». Фото окопних свічок",
+  },
+  {
+    preview: Candle_08_prev,
+    original: Candle_08_orig,
+    description: "БФ «Заново». Фото окопних свічок",
+  },
+  {
+    preview: Candle_09_prev,
+    original: Candle_09_orig,
+    description: "БФ «Заново». Фото окопних свічок",
+  },
+  {
+    preview: Candle_10_prev,
+    original: Candle_10_orig,
+    description: "БФ «Заново». Фото окопних свічок",
+  },
+  {
+    preview: Candle_11_prev,
+    original: Candle_11_orig,
+    description: "БФ «Заново». Фото окопних свічок",
+  },
+  {
+    preview: Candle_12_prev,
+    original: Candle_12_orig,
+    description: "БФ «Заново». Фото окопних свічок",
+  },
+  {
+    preview: Candle_13_prev,
+    original: Candle_13_orig,
+    description: "БФ «Заново». Фото окопних свічок",
+  },
+  {
+    preview: Candle_14_prev,
+    original: Candle_14_orig,
+    description: "БФ «Заново». Фото окопних свічок",
+  },
+  {
+    preview: Candle_15_prev,
+    original: Candle_15_orig,
+    description: "БФ «Заново». Фото окопних свічок",
+  },
+  {
+    preview: Candle_16_prev,
+    original: Candle_16_orig,
+    description: "БФ «Заново». Фото окопних свічок",
+  },
+  {
+    preview: Candle_17_prev,
+    original: Candle_17_orig,
     description: "БФ «Заново». Фото окопних свічок",
   },
 ]
